08fad91a5488606363d4033f3323156d8564c7b2
Carl Du <carldu@rektec.com.cn>
2021-03-06 16:41:03 +0800

    添加判断类型函数

--- src/common.js
+++ src/common.js
@@ -1,12 +1,51 @@
 /**
- * 判读传入的对象是否为 undefined 或者 null
+ * 返回对象是否是 undefined 或者 null
  * @param {Object} value
  * @returns {boolean}
+ */
+export function isNil(value) {
+  return value === undefined || value === null;
+}
+
+/**
+ * 返回对象是否是Null
+ * @param {Object} value
+ * @returns
  */
 export function isNull(value) {
   return value === undefined || value === null;
 }
 
+/**
+ * 返回对象是否是Number
+ * @param {Object} value
+ * @returns
+ */
+export function isNumber(value) {
+  return (
+    typeof value === 'number' ||
+    Object.prototype.toString.call(value) === '[Object Number]'
+  );
+}
+
+/**
+ * 返回对象是否是Object
+ * @param {Object} value
+ * @returns
+ */
+export function isObject(value) {
+  const type = typeof value;
+  return value != null && (type === 'object' || type === 'function');
+}
+
+/**
+ * 返回对象是否是函数
+ * @param {Object} value
+ * @returns
+ */
+export function isFunction(value) {
+  return typeof value === 'function';
+}
 /**
  * 判断传入对象是否空
  */
