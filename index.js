cfca7212094b0f7a25eff3389f9cb20df0a1b867
carl <18556906294@163.com>
2021-03-15 22:10:49 +0800

    添加socket导出

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import encrypt from './src/utils/encrypt';
 import uuid from './src/utils/uuid';
 import * as http from './src/http';
 import * as common from './src/common';
+import Socket from './src/socket';
 import './src/extension';
 
-export { encrypt, uuid, http, common };
+export { encrypt, uuid, http, common, Socket };
