ee761d1b088024a52771d13ad90c2143b5892343
Karl Du <18556906294@163.com>
2022-11-12 11:50:26 +0800

    v1.0.1

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,24 @@
-import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 
-export default [  {
-  input: './index.ts',
-  output: {
-    dir: 'dist',
-    format: 'cjs',
-    entryFileNames: '[name].cjs.js',
-  },
-  plugins: [resolve(), commonjs(), typescript()],
-}, {
-  input: './index.ts',
-  output: {
-    dir: 'dist',
-    format: 'esm',
-    entryFileNames: '[name].esm.js',
-  },
-  plugins: [resolve(), commonjs(), typescript()],
-}];
+export default [
+  {
+    input: './index.ts',
+    plugins: [json(), typescript({
+      exclude: "node_modules/**",
+      typescript: require("typescript"),
+    })],
+    output: [
+      {
+        dir: 'dist',
+        format: 'cjs',
+        entryFileNames: '[name].cjs.js'
+      },
+      {
+        dir: 'dist',
+        format: 'esm',
+        entryFileNames: '[name].esm.js'
+      }
+    ]
+  }
+];
